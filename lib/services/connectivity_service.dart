fc719f65ae6f4073105fc94605df1f79c4f8afa9
klarah100 <claramapanga7@gmail.com>
2026-09-23 06:48:52 +0200

    feat: add connectivity monitoring

---
+++ lib/services/connectivity_service.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class ConnectivityService {
+  final Connectivity _connectivity = Connectivity();
+  final StreamController<bool> _online = StreamController<bool>.broadcast();
+
+  Stream<bool> get online => _online.stream;
+
+  Future<bool> isOnline() async {
+    final results = await _connectivity.checkConnectivity();
+    return results.any((result) => result != ConnectivityResult.none);
+  }
+
+  Future<void> start() async {
+    _online.add(await isOnline());
+    _connectivity.onConnectivityChanged.listen((results) {
+      _online.add(results.any((result) => result != ConnectivityResult.none));
+    });
+  }
+
+  void dispose() => _online.close();
+}
